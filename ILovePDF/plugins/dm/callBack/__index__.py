9b18d6c0aa4496d79d6d7e7740c0a3cd0e35ea5c
tajabar <121495180+tajabar@users.noreply.github.com>
2023-10-17 14:39:14 +0300

    Update __index__.py

--- ILovePDF/plugins/dm/callBack/__index__.py
+++ ILovePDF/plugins/dm/callBack/__index__.py
@@ -33,7 +33,8 @@ async def __index__(bot, callbackQuery):
         ):
             await work.work(callbackQuery, "delete", False)
             return await callbackQuery.message.reply_text(
-                "#old_queue 💔\n\n`try by sending new file`", reply_markup=_, quote=True
+                "#old_queue 💔\n\n`try by sending new file`",
+                reply_markup=_, quote=True
             )
 
         elif data == "rot360":
@@ -110,7 +111,7 @@ async def __index__(bot, callbackQuery):
             notExit, imageList = await pdfToImages.askimageList(
                 bot,
                 callbackQuery,
-                question=CHUNK["askImage"],
+                question=CHUNK["pyromodASK_4"],
                 limit=int(callbackQuery.message.text.split("•")[1])
                 if "•" in callbackQuery.message.text
                 else 1000,
@@ -141,7 +142,7 @@ async def __index__(bot, callbackQuery):
                 return await watermark.reply(CHUNK["exit"], quote=True)
         elif data == "partPDF":
             notExit, splitData = await partPDF.askPartPdf(
-                bot, callbackQuery, question=CHUNK["pyromodASK_4"],
+                bot, callbackQuery, question=CHUNK["askImage"],
                 limit=int(callbackQuery.message.text.split("•")[1])
                 if "•" in callbackQuery.message.text else None
             )
@@ -222,7 +223,9 @@ async def __index__(bot, callbackQuery):
             )
 
         elif data == "ocr":
-            isSuccess, output_file = await ocrPDF.ocrPDF(input_file=input_file)
+            isSuccess, output_file = await ocrPDF.ocrPDF(
+                input_file=input_file,  cDIR=cDIR
+            )
 
         elif data == "baw":
             isSuccess, output_file = await blackAndWhitePdf.blackAndWhitePdf(
